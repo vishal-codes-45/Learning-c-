8aa55d4d530c6928d1b87221afa3b5d5a6a780c8
vishal-codes-45 <vishalshukla5655@gmail.com>
2026-06-29 12:11:41 +0530

    Add Students-details.c to display student info

---
+++ Students-details.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 17;
+    float height = 5.8;
+    char grade = 'A';
+    char name[] = "Vishal";
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Height: %f\n", height);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
