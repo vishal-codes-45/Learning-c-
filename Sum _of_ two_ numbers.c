b16f8a27fe7022bf0e7477ad637105ce03ad7949
vishal-codes-45 <vishalshukla5655@gmail.com>
2026-06-30 11:47:23 +0530

    Add program to input and display age and height

---
+++ Sum _of_ two_ numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+  int age;
+  float height;
+  printf("apni age likho\n");
+  scanf("%d", &age);
+  printf("tumhari age hai,%d", age);
+
+
+  printf("apni height likho\n");
+  scanf("%f", &height);
+  printf("tumhari height hai,.%.2f" , height);
+  return 0;
+}
