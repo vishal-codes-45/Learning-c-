554f7d55471a96bde189ae7b1a6c6d497b11db1c
vishal-codes-45 <vishalshukla5655@gmail.com>
2026-07-14 19:11:44 +0530

    Add Escape_sequences.c to display user information

---
+++ Escape_sequences.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name:\tVishal\n");
+    printf("Age:\t17\n");
+    printf("Course:\tC Programming\n");
+    printf("Learning \"GitHub\" and C Language\n");
+
+    return 0;
+}
