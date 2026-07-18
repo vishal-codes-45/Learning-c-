17eb2906625103c98db73e72bcc98b28f7849061
vishal-codes-45 <vishalshukla5655@gmail.com>
2026-07-18 17:36:39 +0530

    Add multiplication table program in _loop.c

---
+++ _loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i = 1;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    while(i <= 10)
+    {
+        printf("%d x %d = %d\n", n, i, n*i);
+        i++;
+    }
+
+    return 0;
+}
