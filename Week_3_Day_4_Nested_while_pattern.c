4a5e906d84fa1e42e4811eccefff9fe1f97a77ca
vishal-codes-45 <vishalshukla5655@gmail.com>
2026-07-23 20:07:00 +0530

    Add nested while loop pattern printing
    Implement nested while loops to print a pattern of asterisks.

---
+++ Week_3_Day_4_Nested_while_pattern.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    while (i <= 5) {
+        int j = 1;
+
+        while (j <= i) {
+            printf("* ");
+            j++;
+        }
+
+        printf("\n");
+        i++;
+    }
+
+    return 0;
+}
