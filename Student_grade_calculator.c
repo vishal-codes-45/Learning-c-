d7e5806d18919029b390652eee4aa53a99bf8c2e
vishal-codes-45 <vishalshukla5655@gmail.com>
2026-08-08 21:04:05 +0530

    Add Student grade calculator program

---
+++ Student_grade_calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 90)
+        printf("Grade: A+\n");
+    else if (marks >= 80)
+        printf("Grade: A\n");
+    else if (marks >= 70)
+        printf("Grade: B\n");
+    else if (marks >= 60)
+        printf("Grade: C\n");
+    else if (marks >= 50)
+        printf("Grade: D\n");
+    else
+        printf("Grade: F\n");
+
+    return 0;
+} 
