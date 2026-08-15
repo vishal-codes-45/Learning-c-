0147c5e4e2e51876b2c4843b958abd2cab49649a
vishal-codes-45 <vishalshukla5655@gmail.com>
2026-08-15 20:57:32 +0530

    Create Independence_day.c

---
+++ Independence_day.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("================================\n");
+    printf("      HAPPY INDEPENDENCE DAY\n");
+    printf("================================\n");
+    printf("          JAI HIND 🇮🇳\n");
+    printf("     VANDE MATARAM 🇮🇳\n");
+    printf("================================\n");
+
+    return 0;
+}
